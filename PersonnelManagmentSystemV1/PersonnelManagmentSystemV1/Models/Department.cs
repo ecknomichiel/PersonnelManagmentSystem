71c1161abfebed74757a652aa049540d590bf792
name <wille.multi@live.se>
2017-07-21 15:05:15 +0200

    Created Department model

---
+++ PersonnelManagmentSystemV1/PersonnelManagmentSystemV1/Models/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonnelManagmentSystemV1.Models
+{
+    public class Department
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        [Required]
+        [Display(Name = "Department Name")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Company Name")]
+        public string CompName { get; set; }
+
+        public virtual ICollection<ApplicationUser> Departments { get; set; }
+    }
+}
