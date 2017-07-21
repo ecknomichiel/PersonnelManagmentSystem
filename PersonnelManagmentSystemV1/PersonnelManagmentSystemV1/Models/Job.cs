14b439300b3a4cdefed230a1d26dda5e2b7757d6
Keuthonymous <mikie.daughtrey@gmail.com>
2017-07-21 14:24:40 +0200

    Added model for job

---
+++ PersonnelManagmentSystemV1/PersonnelManagmentSystemV1/Models/Job.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PersonnelManagmentSystemV1.Models
+{
+    public class Job
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Job title")]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Job description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Company Name")]
+        public string CompName { get; set; }
+
+        public virtual ICollection<ApplicationUser> Applicants { get; set; }
+    }
+}
